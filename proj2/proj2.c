0a91a0736dbf03bbaaf3e547066f1661541135cc
Marek Gergel <mage26gg@gmail.com>
2023-04-29 05:22:21 +0200

    proj2 almost done
    segfault issue

--- proj2/proj2.c
+++ proj2/proj2.c
@@ -16,16 +16,14 @@
 #include <sys/mman.h>
 #include <unistd.h>
 #include <time.h>
-#include <errno.h>
-#include <fcntl.h>
 #include <semaphore.h>
 #include <ctype.h>
 
 #define PARAMS 5 // number of required values in arguments {NZ, NU, TZ, TU, F}
 #define OUT_FILE "proj2.out"
 
-#define KEY_COUNTER 1
-#define KEY_OFFICE_CLOSED 12
+#define KEY_COUNTER 1234
+#define KEY_OFFICE_CLOSED 12345
 
 // shared memory
 int counter_shmid;
@@ -34,6 +32,16 @@ int *counter;
 int office_closed_shmid;
 bool *office_closed;
 
+// semaphores
+sem_t *sem_called_list;
+sem_t *sem_called_package;
+sem_t *sem_called_money;
+sem_t *sem_output;
+
+// output file
+FILE *f = NULL;
+
+// services
 typedef enum service
 {
     LIST = 1,
@@ -41,17 +49,43 @@ typedef enum service
     MONEY = 3
 } service_t;
 
+// queue
+typedef struct node
+{
+    unsigned long data;
+    struct node *next;
+} node_t;
+
+typedef struct queue
+{
+    node_t *head;
+    node_t *tail;
+} queue_t;
+
+queue_t *queue_list;
+queue_t *queue_package;
+queue_t *queue_money;
+
 // process functions
 void officer_p(unsigned long id, unsigned long pause);
 void customer_p(unsigned long id, unsigned long wait);
 
+// semaphores functions
+void sems_init();
+void sems_destroy();
+
 // queue functions
-bool queue_is_empty();
+void queue_init(queue_t *queue);
+void queue_destroy(queue_t *queue);
+void queue_enqueue(queue_t *queue, unsigned long data);
+unsigned long queue_dequeue(queue_t *queue);
+bool queue_is_empty(queue_t *queue);
+bool queue_is_empty_s(service_t service);
 
 // helper functions
 void append_to_file(const char *fmt, ...);
 void free_resources();
-void error_exit(int errcode, const char *fmt, ...);
+void error_exit(bool free, int errcode, const char *fmt, ...);
 
 /**
  * @brief Main process - creates customers and officers, controls office
@@ -65,16 +99,14 @@ int main(int argc, char *argv[])
     // invalid number of arguments
     if (argc - 1 != PARAMS)
     {
-        error_exit(1, "Invalid number of argumets\n");
+        error_exit(false, 1, "Invalid number of argumets\n");
     }
 
-    // create/clean output file
-    FILE *f = fopen(OUT_FILE, "w");
-    if (f == NULL)
+    // open output file
+    if ((f = fopen(OUT_FILE, "w")) == NULL)
     {
-        error_exit(2, "Cannot open file '%s'\n", OUT_FILE);
+        error_exit(false, 2, "Cannot open file '%s'\n", OUT_FILE);
     }
-    fclose(f);
 
     // convert arguments into ulong value
     unsigned long param[PARAMS];
@@ -84,21 +116,26 @@ int main(int argc, char *argv[])
         param[i] = strtoul(argv[i + 1], &ptr, 0);
         if (strcmp(ptr, "") != 0)
         {
-            error_exit(1, "Argument %d. '%s' is not a number\n", i + 1, argv[i + 1]);
+            error_exit(false, 1, "Argument %d. '%s' is not a number\n", i + 1, argv[i + 1]);
         }
         else if ((i == 0 || i == 1) && (param[i] == 0 || argv[i + 1][0] == '-'))
         {
-            error_exit(1, "Argument %d. '%s' must be bigger than 0\n", i + 1, argv[i + 1]);
+            error_exit(false, 1, "Argument %d. '%s' must be bigger than 0\n", i + 1, argv[i + 1]);
         }
         else if ((i == 2 || i == 4) && (param[i] > 10000 || argv[i + 1][0] == '-'))
         {
-            error_exit(1, "Argument %d. '%s' must be in range 0 and 10000\n", i + 1, argv[i + 1]);
+            error_exit(false, 1, "Argument %d. '%s' must be in range 0 and 10000\n", i + 1, argv[i + 1]);
         }
         else if ((i == 3) && (param[i] > 100 || argv[i + 1][0] == '-'))
         {
-            error_exit(1, "Argument %d. '%s' must be in range 0 and 100\n", i + 1, argv[i + 1]);
+            error_exit(false, 1, "Argument %d. '%s' must be in range 0 and 100\n", i + 1, argv[i + 1]);
         }
     }
+
+    // init queues
+    queue_init(queue_list);
+    queue_init(queue_package);
+    queue_init(queue_money);
 
     // create shared memory
     counter_shmid = shmget(KEY_COUNTER, sizeof(int), IPC_CREAT | 0666);
@@ -109,8 +146,14 @@ int main(int argc, char *argv[])
     office_closed = (bool *)shmat(office_closed_shmid, NULL, 0);
     *office_closed = false;
 
+    // create semaphores
+    sems_init();
+
+    // post semaphores
+    sem_post(sem_output);
+
     // seed the random number generator with the current time
-    srand(time(NULL));
+    srand(getpid() * time(NULL));
 
     // customer generator
     for (unsigned long i = 0; i < param[0]; i++)
@@ -119,11 +162,10 @@ int main(int argc, char *argv[])
         if (customer == 0)
         {
             customer_p(i, param[2]);
-            exit(0);
         }
         else if (customer < 0)
         {
-            error_exit(1, "Cannot create customer process\n");
+            error_exit(true, 1, "Cannot create customer process\n");
         }
     }
 
@@ -134,11 +176,10 @@ int main(int argc, char *argv[])
         if (officer == 0)
         {
             officer_p(i, param[3]);
-            exit(0);
         }
         else if (officer < 0)
         {
-            error_exit(1, "Cannot create officer process\n");
+            error_exit(true, 1, "Cannot create officer process\n");
         }
     }
 
@@ -172,7 +213,7 @@ void customer_p(unsigned long id, unsigned long wait)
     append_to_file("Z %d: started\n", id);
 
     // seed the random number generator with the current time
-    srand(time(NULL));
+    srand(getpid() * time(NULL));
 
     // sleep for <0, TZ> milliseconds
     usleep(rand() % wait * 1000);
@@ -183,7 +224,21 @@ void customer_p(unsigned long id, unsigned long wait)
 
         append_to_file("Z %d: entering office for a service %d\n", id, service);
 
-        // TODO wait for called by officer
+        switch (service)
+        {
+        case LIST:
+            queue_enqueue(queue_list, id);
+            sem_wait(sem_called_list);
+            break;
+        case PACKAGE:
+            queue_enqueue(queue_package, id);
+            sem_wait(sem_called_package);
+            break;
+        case MONEY:
+            queue_enqueue(queue_money, id);
+            sem_wait(sem_called_money);
+            break;
+        }
 
         append_to_file("Z %d: called by office worker\n", id);
 
@@ -192,6 +247,8 @@ void customer_p(unsigned long id, unsigned long wait)
     }
 
     append_to_file("Z %d: going home\n", id);
+
+    exit(0);
 }
 
 /**
@@ -204,15 +261,69 @@ void officer_p(unsigned long id, unsigned long pause)
     append_to_file("U %d: started\n", id);
 
     // seed the random number generator with the current time
-    srand(time(NULL));
+    srand(getpid() * time(NULL));
 
     while (1)
     {
 
-        // TODO check any customer is waiting
-        if (!queue_is_empty())
+        if (!queue_is_empty(queue_list) || !queue_is_empty(queue_package) || !queue_is_empty(queue_money))
         {
-            (void)0;
+            service_t service, service1, service2, service3;
+
+            service1 = (rand() % 3) + 1;
+            service = service1;
+
+            if (queue_is_empty_s(service))
+            {
+                do
+                {
+                    service2 = (rand() % 3) + 1;
+                } while (service2 == service1);
+                service = service2;
+
+                if (queue_is_empty_s(service))
+                {
+                    do
+                    {
+                        service3 = (rand() % 3) + 1;
+                    } while (service3 == service1 || service3 == service2);
+                    service = service3;
+
+                    if (queue_is_empty_s(service))
+                    {
+                        continue;
+                    }
+                }
+            }
+
+            switch (service)
+            {
+            case LIST:
+                if (!queue_is_empty(queue_list))
+                {
+                    sem_post(sem_called_list);
+                }
+                break;
+            case PACKAGE:
+                if (!queue_is_empty(queue_package))
+                {
+                    sem_post(sem_called_package);
+                }
+                break;
+            case MONEY:
+                if (!queue_is_empty(queue_money))
+                {
+                    sem_post(sem_called_money);
+                }
+                break;
+            }
+
+            append_to_file("U %d: serving a service of type %d\n", id, service);
+
+            // sleep for <0, 10>
+            usleep(rand() % 11);
+
+            append_to_file("U %d: service finished\n", id);
         }
         else
         {
@@ -230,12 +341,122 @@ void officer_p(unsigned long id, unsigned long pause)
     }
 
     append_to_file("U %d: going home\n", id);
+
+    exit(0);
 }
+
+// ---------------------- SEMAPHORE FUNCTIONS -----------------------
+
+void sems_init()
+{
+    sem_called_list = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    sem_called_package = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    sem_called_money = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    sem_output = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+
+    if (sem_init(sem_called_list, 1, 0) == -1 || sem_init(sem_called_package, 1, 0) == -1 || sem_init(sem_called_money, 1, 0) == -1 || sem_init(sem_output, 1, 0) == -1)
+    {
+        error_exit(true, 1, "Cannot initialize semaphore\n");
+    }
+}
+
+void sems_destroy()
+{
+    int error = 0;
+
+    error |= sem_destroy(sem_called_list) | sem_destroy(sem_called_package) | sem_destroy(sem_called_money) | sem_destroy(sem_output);
+    error |= munmap(sem_called_list, sizeof(sem_t)) | munmap(sem_called_package, sizeof(sem_t)) | munmap(sem_called_money, sizeof(sem_t)) | munmap(sem_output, sizeof(sem_t));
+
+    if (error != 0)
+    {
+        error_exit(false, 1, "Cannot destroy semaphore\n");
+    }
+}
+
 // ------------------------ QUEUE FUNCTIONS -------------------------
 
-bool queue_is_empty()
+void queue_init(queue_t *queue)
 {
-    return true;
+    queue = (queue_t *)malloc(sizeof(queue_t));
+    if (queue == NULL)
+    {
+        error_exit(true, 1, "Cannot allocate memory for queue\n");
+    }
+
+    queue->head = NULL;
+    queue->tail = NULL;
+}
+
+void queue_destroy(queue_t *queue)
+{
+    while (!queue_is_empty(queue))
+    {
+        node_t *tmp = queue->head;
+        queue->head = queue->head->next;
+        free(tmp);
+    }
+
+    free(queue);
+}
+
+void queue_enqueue(queue_t *queue, unsigned long data)
+{
+    node_t *node = (node_t *)malloc(sizeof(node_t));
+    if (node == NULL)
+    {
+        error_exit(true, 1, "Cannot allocate memory for node\n");
+    }
+
+    node->data = data;
+    node->next = NULL;
+
+    if (queue_is_empty(queue))
+    {
+        queue->head = node;
+        queue->tail = node;
+    }
+    else
+    {
+        queue->tail->next = node;
+        queue->tail = node;
+    }
+}
+
+unsigned long queue_dequeue(queue_t *queue)
+{
+    if (queue_is_empty(queue))
+    {
+        error_exit(true, 1, "Cannot dequeue from empty queue\n");
+    }
+
+    node_t *tmp = queue->head;
+    unsigned long data = tmp->data;
+
+    queue->head = queue->head->next;
+    free(tmp);
+
+    return data;
+}
+
+bool queue_is_empty(queue_t *queue)
+{
+    return queue->head == NULL;
+}
+
+bool queue_is_empty_s(service_t service)
+{
+    switch (service)
+    {
+    case LIST:
+        return queue_is_empty(queue_list);
+    case PACKAGE:
+        return queue_is_empty(queue_package);
+    case MONEY:
+        return queue_is_empty(queue_money);
+    default:
+        error_exit(true, 1, "Invalid service type\n");
+        return true;
+    }
 }
 
 // ------------------------ HELPER FUNCTIONS ------------------------
@@ -247,25 +468,16 @@ bool queue_is_empty()
  */
 void append_to_file(const char *fmt, ...)
 {
-    // Open file for appending
-    FILE *f = fopen(OUT_FILE, "a");
-    if (f == NULL)
-    {
-        error_exit(1, "Cannot open file '%s'\n", OUT_FILE);
-    }
+    sem_wait(sem_output);
 
-    // Disable buffering
-    // setbuf(f, NULL);
-
-    // Print message
     va_list args;
     va_start(args, fmt);
     fprintf(f, "%d: ", ++(*counter));
     vfprintf(f, fmt, args);
     va_end(args);
+    fflush(f);
 
-    // Close file
-    fclose(f);
+    sem_post(sem_output);
 }
 
 /**
@@ -273,24 +485,33 @@ void append_to_file(const char *fmt, ...)
  */
 void free_resources()
 {
-    shmdt(counter);                           // Detach from shared memory
-    shmctl(counter_shmid, KEY_COUNTER, NULL); // Remove shared memory
+    // Close output file
+    fclose(f);
 
-    shmdt(office_closed);                                 // Detach from shared memory
-    shmctl(office_closed_shmid, KEY_OFFICE_CLOSED, NULL); // Remove shared memory
+    // Destroy queue
+    queue_destroy(queue_list);
+    queue_destroy(queue_package);
+    queue_destroy(queue_money);
+
+    // Detach and remove shared memory
+    shmdt(counter);
+    shmctl(counter_shmid, KEY_COUNTER, NULL);
+    shmdt(office_closed);
+    shmctl(office_closed_shmid, KEY_OFFICE_CLOSED, NULL);
+
+    // Destroy semaphores
+    sems_destroy();
 }
 
 /**
  * @brief Print error message and exit with error code
+ * @param free boolean value indicating whether to free resources
  * @param errcode error code
  * @param fmt format string
  * @param ... arguments
  */
-void error_exit(int errcode, const char *fmt, ...)
+void error_exit(bool free, int errcode, const char *fmt, ...)
 {
-    fflush(stderr);
-
-    // Print error message
     va_list args;
     va_start(args, fmt);
     fprintf(stderr, "Error : ");
@@ -298,9 +519,10 @@ void error_exit(int errcode, const char *fmt, ...)
     va_end(args);
     fflush(stderr);
 
-    // Free resources
-    free_resources();
+    if (free)
+    {
+        free_resources();
+    }
 
-    // Exit with error code
     exit(errcode);
 }
